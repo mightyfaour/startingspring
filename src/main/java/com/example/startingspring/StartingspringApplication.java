50ddba68a813ac1c04d11b2fab81846b005e0370
techub <tech4phil@outlook.com>
2022-10-26 16:20:03 +0100

    Initial commit

---
+++ src/main/java/com/example/startingspring/StartingspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.startingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartingspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/startingspring/StartingspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartingspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
